9d3d1eb0baa22776931c135aef83122f40f760ba
Juan David Merchan Torres <juan1014166@gmail.com>
2021-12-19 16:44:23 -0500

    feat(img): Add emit error & emit loaded

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-img',
@@ -7,7 +7,19 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ImgComponent implements OnInit {
   @Input() imgUrl: string = 'initial value';
+  @Output() loaded = new EventEmitter<string>();
+  imgDefault: string = './assets/images/default.png';
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  imgError(): void {
+    this.imgUrl = this.imgDefault;
+  }
+
+  imgLoaded(): void {
+    console.log('Log child');
+    this.loaded.emit(this.imgUrl);
+  }
 }
