e6560e13cb6cf61f7221eec145210a5bc90d1a5d
Juan David Merchan Torres <juan1014166@gmail.com>
2021-12-19 20:47:18 -0500

    feat(app): Add render product list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Product } from './components/product/product.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,6 +9,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   imgUrl: string = 'https://www.w3schools.com/howto/img_avatar.png';
+  products: Product[] = [
+    {
+      id: '1',
+      name: 'El mejor juguete',
+      price: 565,
+      image: './assets/images/toy.jpg',
+    },
+    {
+      id: '2',
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/bike.jpg',
+    },
+    {
+      id: '3',
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: './assets/images/album.jpg',
+    },
+    {
+      id: '4',
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/books.jpg',
+    },
+  ];
 
   onLoaded(imgUrl: string): void {
     console.log('Log Parent', imgUrl);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ImgComponent } from './components/img/img.component';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
-  declarations: [AppComponent, ImgComponent],
+  declarations: [AppComponent, ImgComponent, ProductComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
